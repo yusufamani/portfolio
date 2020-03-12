0e3080651b33c8f0514010519cdf1b7609732818
Yusuf Amani <yusufamani@csus.edu>
2020-03-12 13:40:20 -0700

    made some changes in index and php file

--- contactform.php
+++ contactform.php
@@ -4,7 +4,7 @@
 	$visitor_email =  $_POST['email'];
 	$message = $_POST['message'];
 
-	$email_from = 'yusufamani108@gmail.com';
+	$email_from = 'yusufamani@yahoo.com';
 
 	$email_subject = 'New Form Submission';
 
